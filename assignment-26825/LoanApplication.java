83ec39912fefd98f8a90025c9657c94f46350b6d
pranav-8660 <pranavcse2003@gmail.com>
2025-08-26 05:11:16 +0000

    create abstract class LoanApplication

---
+++ assignment-26825/LoanApplication.java
@@ -0,0 +1,38 @@
+public abstract class LoanApplication {
+    
+    private static int counter = 1000;
+    protected int applicationId;
+    protected String customerName;
+    protected double amount;
+    protected LoanType type;
+    protected LoanStatus status;
+
+    public LoanApplication(String customerName, double amount, LoanType type) {
+        this.applicationId = counter++;
+        this.customerName = customerName;
+        this.amount = amount;
+        this.type = type;
+        this.status = null;
+    }
+
+    public abstract boolean validateApplication();
+
+    public void apply() {
+        this.status = LoanStatus.PENDING;
+    }
+
+    @Override
+    public String toString() {
+        return "LoanApplication{" +
+                "ID=" + applicationId +
+                ", Name='" + customerName + '\'' +
+                ", Amount=" + amount +
+                ", Type=" + type +
+                ", Status=" + status +
+                '}';
+    }
+
+    static {
+        System.out.println("SmartLoan System initialized.");
+    }
+}
